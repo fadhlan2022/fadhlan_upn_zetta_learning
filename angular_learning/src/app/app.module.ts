3568c8d909abce51a280b6cd1d294a554637e9e2
fadhlan2022 <fadhlanhisyam2022@gmail.com>
2022-11-01 14:14:15 +0700

    commit-modul

--- angular_learning/src/app/app.module.ts
+++ angular_learning/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Modul1Component } from './modul1/modul1.component';
+import { Modul2Component } from './modul2/modul2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Modul1Component,
+    Modul2Component
   ],
   imports: [
     BrowserModule,
