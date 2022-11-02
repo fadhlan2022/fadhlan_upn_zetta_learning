66c0c5d5184da690fafcd93f438666a4ced7d9ce
fadhlan2022 <fadhlanhisyam2022@gmail.com>
2022-11-02 15:16:06 +0700

    day2

---
+++ angular_learning/angular_learning/src/app/modul1/modul1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modul1',
+  templateUrl: './modul1.component.html',
+  styleUrls: ['./modul1.component.scss']
+})
+export class Modul1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
